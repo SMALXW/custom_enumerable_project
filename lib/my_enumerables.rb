35f3e5e803a63944003bf137a70fb7ad1d3e3a01
smalxw <samuel.a.walton@gmail.com>
2023-12-09 10:54:07 +0000

    Completed the my_each method on the array class

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,11 +1,21 @@
 module Enumerable
   # Your code goes here
-end
 
 # You will first have to define my_each
 # on the Array class. Methods defined in
 # your enumerable module will have access
 # to this method
+end
 class Array
   # Define my_each here
+  def my_each
+    if block_given?
+      i = 0
+      until i == self.length do
+        yield(self[i])
+        i += 1
+      end
+    end
+    self
+  end
 end
