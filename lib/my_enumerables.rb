17ad77f894a7059da865e90f2d4c9ad0cf412bc5
smalxw <samuel.a.walton@gmail.com>
2023-12-11 12:45:07 +0000

    Completed a few more functions

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -17,6 +17,67 @@ module Enumerable
     self
   end
 
+  def my_select
+    results = []
+    my_each { |element| results.push(element) if yield element }
+    results
+  end
+
+  def my_all?
+    if block_given?
+      result = true
+      my_each { |i| result = false if yield(i) == false}
+      result
+    else
+      self
+    end
+  end
+
+  def my_any?
+    if block_given?
+      result = false
+      my_each { |i| result = true if yield(i) == true}
+      result
+    else
+      self
+    end
+  end
+
+  def my_none?
+    if block_given?
+      result = true
+      my_each { |i| result = false if yield(i)}
+      result 
+    else
+      self
+    end
+  end
+
+  def my_count
+    count = 0
+    if block_given?
+      my_each { |i| count+=1 if yield(i) == true}
+      count
+    else 
+      my_each { |i| count+=1}
+      count
+    end
+  end
+
+  def my_map
+    if block_given?
+      result = []
+      my_each do |i| 
+        result.push(yield(i))
+      end
+      result
+    else
+      self
+    end
+  end
+
+
+
 
 end
 class Array
